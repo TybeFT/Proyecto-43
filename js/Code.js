18f920d1b5ac8837ded7fd27563d6c6682cd449c
TybeFT <77138543+TybeFT@users.noreply.github.com>
2022-01-15 03:03:32 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    // VAriable
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: It is used to store code data", 100,70)
+    // Base de datos 
+    fill("white")
+    text("N I T O C U N F",560,150)
+    fill("lightblue")
+    text("Hint: Stores multiple code instructions to perform a specific action",560,170)
+    // Funcion
+    fill("white")
+    text("T A B E S A D A",100,320)
+    fill("lightblue")
+    text("Hint: You can save much data and actualizate in live",100,340)
+
+}
